00e46d3ef4901d86f907b7f4ff8b7521fb100cb8
mc <162787712+mc-str@users.noreply.github.com>
2026-05-23 12:49:05 +0800

    Add files via upload

---
+++ icons.config.js
@@ -0,0 +1,11 @@
+const ICON_CONFIGS = [
+  { file: 'icon_1.png', name: '图标1' },
+  { file: 'icon_2.png', name: '图标2' },
+  { file: 'icon_3.png', name: '图标3' },
+  { file: 'icon_4.png', name: '图标4' },
+  { file: 'icon_5.png', name: '图标5' },
+  { file: 'icon_6.png', name: '图标6' },
+  { file: 'icon_7.png', name: '图标7' },
+  { file: 'icon_8.png', name: '图标8' },
+  { file: 'icon_9.png', name: '图标9' }
+];
